38800cde8dd950095a74b8ec2c2b1457e36a3e5a
Roey Borsteinas <roeyb1@hotmail.com>
2024-01-14 10:26:46 -0500

    Implemented rudimentary request caching system to avoid spamming api

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::cmp::min;
-use std::iter::Map;
+use std::collections::HashMap;
+use std::fs;
 use std::sync::Arc;
 use serde::{Deserialize, Serialize};
 use reqwest::{Error, Url};
-use reqwest::cookie::{CookieStore, Jar};
-use reqwest::header::USER_AGENT;
-use serde_json::from_value;
+use reqwest::cookie::Jar;
 use tokio::runtime::Runtime;
 
 static POE_ENDPOINT: &str = "https://api.pathofexile.com";
@@ -93,6 +91,12 @@ struct TradeExchangeResponse {
     total: u32
 }
 
+impl From<TradeExchangeResponse> for Vec<TradeExchangeResult> {
+    fn from(response: TradeExchangeResponse) -> Self {
+        response.result.into_iter().filter_map(|(_, value)| serde_json::from_value(value).ok()).collect()
+    }
+}
+
 fn build_client(request_url: &Url) -> Result<reqwest::Client, Error> {
     // #todo: POESESSID should be stored and read from a config file. for now just use env vars to keep this private during development
     let cookie = format!("{}={}", "POESESSID", std::env::var("POESESSID").unwrap());
@@ -116,6 +120,39 @@ async fn get_leagues() -> Result<Leagues, Error> {
 }
 
 async fn get_bulk_results(have: &str, want: &str) -> Result<Vec<TradeExchangeResult>, Error> {
+    // check the cache before submitting a web request to limit API calls.
+    let cache_string =  fs::read_to_string("cache/requests.csv");
+    match cache_string {
+        Ok(cache_data) => {
+            // hashmap with have/want concatenated as the key
+            let mut cache: HashMap<String, String> = HashMap::new();
+            for line in cache_data.split("\n") {
+                let mut index = 0;
+                let mut key: String = "".to_string();
+                let mut value: String = "".to_string();
+                for line in line.split(",") {
+                    if index < 2 {
+                        key += line;
+                    } else {
+                        value = line.to_string();
+                    }
+                    index += 1;
+                }
+                cache.insert(key, value);
+            }
+
+            let havewant_pair = format!("{}{}", have, want);
+            if cache.contains_key(&havewant_pair) {
+                log::debug!("Request for {{have: {}, want: {}}} found in cache!", have, want);
+                let response: TradeExchangeResponse = serde_json::from_str(std::str::from_utf8(&base64::decode(cache.get(&havewant_pair).unwrap()).unwrap()).unwrap()).unwrap();
+                let result: Vec<TradeExchangeResult> = response.into();
+
+                return Ok(result);
+            }
+        }
+        _ => {}
+    }
+
     let request_url = format!("{}/{}", TRADE_ENDPOINT, SELECTED_LEAGUE).parse::<Url>().unwrap();
 
     let client = build_client(&request_url)?;
@@ -131,6 +168,8 @@ async fn get_bulk_results(have: &str, want: &str) -> Result<Vec<TradeExchangeRes
         engine: "new".to_string(),
     };
 
+    log::debug!("Submitting request to trade API...");
+
     let response = client
         .post(request_url.clone())
         .header("User-Agent", "KalandraKapital/1.0")
@@ -140,9 +179,11 @@ async fn get_bulk_results(have: &str, want: &str) -> Result<Vec<TradeExchangeRes
 
     let result_text = response.text().await?;
 
+    fs::write("cache/requests.csv", format!("{},{},{}", have, want, base64::encode(&result_text))).expect("Unable to write request data to cache");
+
     let response: TradeExchangeResponse = serde_json::from_str(&result_text).unwrap();
 
-    let result: Vec<TradeExchangeResult> = response.result.into_iter().filter_map(|(_, value)| serde_json::from_value(value).ok()).collect();
+    let result: Vec<TradeExchangeResult> = response.into();
 
     Ok(result)
 }
@@ -166,7 +207,29 @@ fn get_bulk_pricing(have: &str, want: &str) -> f32 {
     min_price
 }
 
+fn setup_environment() -> Result<(), String> {
+    match fs::read_dir("cache") {
+        Ok(_) => { Ok(()) }
+        Err(_) => {
+            match fs::create_dir("cache") {
+                Ok(_) => { Ok(()) }
+                Err(e) => { Err(format!("Failed to create request cache directory ({})", e))}
+            }
+        }
+    }
+}
+
 fn main() {
+    env_logger::init();
+
+    match setup_environment() {
+        Err(error) => {
+            log::error!("Critical setup failure: {}", error);
+            return;
+        },
+        _ => log::info!("Setup complete")
+    };
+
     let rt = Runtime::new().unwrap();
     //let result = rt.block_on(async { get_leagues().await.unwrap() });
 
@@ -181,5 +244,5 @@ fn main() {
 
     let result = get_bulk_pricing("divine", "serrated-fossil");
 
-    println!("It costs {} divine orbs per serrated fossil", result);
+    log::info!("It costs {} divine orbs per serrated fossil", result);
 }
